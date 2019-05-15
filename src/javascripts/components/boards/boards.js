90d176141cf322b7c141bd3cbb12b31a96a7a119
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-05-14 20:09:21 -0500

    pins button functionality

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -3,14 +3,32 @@ import util from '../../helpers/util';
 
 let boards = [];
 
+const seePinDiv = (event) => {
+  const boardId = event.target.closest('.card').id;
+  console.error('button', boardId);
+  document.getElementById('boards-page').classList.add('hide');
+  document.getElementById('pins-page').classList.remove('hide');
+};
+
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-pins');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seePinDiv);
+  }
+};
+
 const domStringBuilder = (array) => {
   let domString = '';
   array.forEach((item) => {
-    domString += '<div class="w-25 p-3">';
-    domString += `<div id=${item.id} class="card"><h3>${item.name}</h3></div>`;
+    domString += '<div class="col-3 p-3">';
+    domString += `<div id=${item.id} class="card">`;
+    domString += `<h3>${item.name}</h3>`;
+    domString += '<button class="btn btn-dark col-3 m-auto see-pins">Pins</button>';
+    domString += '</div>';
     domString += '</div>';
   });
   util.printToDom('user-boards', domString);
+  bindEvents();
 };
 
 const initBoards = () => {
