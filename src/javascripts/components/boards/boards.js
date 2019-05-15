bee8f6a4b63ec4ff5e27a3b803246dc43f64db0c
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-05-14 19:24:57 -0500

    boards are printing

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,24 @@
 import boardsData from '../../helpers/data/boards-data';
+import util from '../../helpers/util';
+
+let boards = [];
+
+const domStringBuilder = (array) => {
+  let domString = '';
+  array.forEach((item) => {
+    domString += '<div class="w-25 p-3">';
+    domString += `<div id=${item.id} class="card"><h3>${item.name}</h3></div>`;
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardsData.laodBoards()
     .then((response) => {
-      console.error(response);
+      const boardsResults = response.data.boards;
+      boards = boardsResults;
+      domStringBuilder(boards);
     })
     .catch(error => console.error(error));
 };
